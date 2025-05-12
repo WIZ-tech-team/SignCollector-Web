8ae65689ac2b89eb1fdedbb90d5a4040c0820486
Saifeddeen <20173065@student.alazhar.edu.ps>
2025-05-12 12:41:48 +0300

    New Updates on Design | 2nd

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Mobile\AuthController;
 use App\Http\Controllers\Mobile\DetailedSignsController;
 use App\Http\Controllers\Mobile\SignsController;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('mobile')->group(function () {
@@ -26,4 +27,11 @@ Route::prefix('mobile')->group(function () {
         Route::post('/', 'store');
         Route::delete('{sign}', 'destroy');
     });
+});
+
+Route::post('/test-size', function($request) {
+    return response()->json([
+        'size' => $request->header('Content-Length'),
+        'max' => ini_get('post_max_size')
+    ]);
 });
