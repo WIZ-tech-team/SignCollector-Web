d6dd20bf5bfd6d89850765f5cbbd58ca5d51539e
Saifeddeen <20173065@student.alazhar.edu.ps>
2025-05-26 21:39:02 +0300

    Mobile: Users Details API

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -16,7 +17,7 @@ class DatabaseSeeder extends Seeder
     {
 
         // Roles-Permissions Seed
-        $this->call(RolesPermissionsSeeder::class);
+        // $this->call(RolesPermissionsSeeder::class);
 
         // Users Seed
         User::whereIn('email', ['super@example.test', 'admin@example.test', 'user@example.test', 'user@example.com'])->forceDelete();
@@ -27,6 +28,7 @@ class DatabaseSeeder extends Seeder
             'type' => 'Super-Admin',
             'phone' => '+2011123456789',
             'password' => Hash::make('password'),
+            'crypt_password' => Crypt::encrypt('password'),
             'created_at' => now(),
             'updated_at' => now(),
         ])->assignRole('SuperAdmin');
@@ -35,6 +37,7 @@ class DatabaseSeeder extends Seeder
             'name'       => 'Test User',
             'email'      => 'user@example.test',
             'password'   => Hash::make('12345678'),
+            'crypt_password'   => Crypt::encrypt('12345678'),
             'type'       => 'User',
             'created_at' => now(),
             'updated_at' => now(),
@@ -46,7 +49,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(RoadsSeeder::class);
 
         // Mobile-Web Users Seed
-        // $this->call(StaticMobileUsersSeeder::class);
+        $this->call(StaticMobileUsersSeeder::class);
 
         // Update roles for mobile users
         $collectors = User::whereIn('email', [
